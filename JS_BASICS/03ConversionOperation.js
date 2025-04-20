42d8b31cb222e731b1c77d3d6c607afb5a798790
vijayvansh <sainivijayvansh@gmail.com>
2025-04-21 00:54:04 +0530

    DataType and DataType Conversion Operations

---
+++ JS_BASICS/03ConversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "33"
+
+// console.log(score + "is type of: " + typeof score);
+
+// let scoreInNumber = Number(score)
+// console.log(scoreInNumber)
+// console.log("Now score is type of: " + typeof scoreInNumber)
+
+// let isLoggedIn
+// let loggedIn = Boolean(isLoggedIn)
+// console.log(loggedIn);
+// console.log(typeof loggedIn);
+
+/*
+    Conversion to Boolean ->
+        1 => true; 0 => false
+        null => false
+        undefined => false
+        "" => false; "someValue" => true
+*/
+
+let someNumber
+
+let convertedString = String(someNumber)
+console.log(convertedString);
+console.log(typeof convertedString);
+
+/*
+    Conversion to String ->
+        anyNumber => string;
+        null => null (string)
+        undefined => undefined (string)
+*/
+
+
