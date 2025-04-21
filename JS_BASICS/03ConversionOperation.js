a1cca004c36f09670de544a6d9d51a27f27062b7
vijayvansh <sainivijayvansh@gmail.com>
2025-04-21 18:17:11 +0530

    Conversion Operation file modified

--- JS_BASICS/03ConversionOperation.js
+++ JS_BASICS/03ConversionOperation.js
@@ -19,11 +19,11 @@
         "" => false; "someValue" => true
 */
 
-let someNumber
+// let someNumber
 
-let convertedString = String(someNumber)
-console.log(convertedString);
-console.log(typeof convertedString);
+// let convertedString = String(someNumber)
+// console.log(convertedString);
+// console.log(typeof convertedString);
 
 /*
     Conversion to String ->
@@ -33,3 +33,33 @@ console.log(typeof convertedString);
 */
 
 
+// *************************  Operations  *************************
+
+let number = 123
+let negNumber = -number
+// console.log(negNumber);
+
+
+// basic operations ->
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);  // ** 2 power 3
+
+
+let str1 = "My Name"
+let str2 = "vijay"
+
+let str3 = str1 + " is " + str2  // add 2 strings with '+' operator
+// console.log(str3);
+
+// Some confussing conversion and operations ->
+console.log(1 + "2");  // => 12
+console.log("1" + 2);  // => 12
+console.log("1" + 2 + 3);  // => 123
+console.log("1" + "2" + 3);  // => 123
+console.log(1 + 2 + "3");   // => 33 (it add (1+2) and convert it into stirng then add two strings ("3" + "3"))
+
+// Not prefer to write this type of low readability code in industry grade projects
