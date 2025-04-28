c7887ea017b5c27e8ea4e2156525e1db3aab94c6
vijayvansh <sainivijayvansh@gmail.com>
2025-04-28 21:03:48 +0530

    If Statement in JS

---
+++ CONTROL_FLOW_04/IfStatement.js
@@ -0,0 +1,12 @@
+// Control Flow Of Program
+
+let googleLogIn = true
+let mailLogIn = false
+
+// if(googleLogIn && mailLogIn){
+//     console.log("User is Logged In")
+// }
+
+// if(googleLogIn) console.log("User Logged In");
+
+
