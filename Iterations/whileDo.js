64b9396d87c495322f10b4ca6805c92cb96a61bb
vijayvansh <sainivijayvansh@gmail.com>
2025-04-29 01:48:46 +0530

    While and Do-while loops in JS

---
+++ Iterations/whileDo.js
@@ -0,0 +1,23 @@
+// While Loop in JS ->
+
+let index = 0;
+while (index <= 10) {
+    // console.log(`Value of Index ${index}`);
+    index = index + 2;
+}
+
+let myArray = ["vijay", "vansh", "saini"]
+let arrIndex = 0;
+while (arrIndex < myArray.length){
+    // console.log(`Array element is: ${myArray[arrIndex]}`);
+    arrIndex = arrIndex + 1;
+}
+
+
+// Do-While Loop in JS ->
+let anArray = [12, 43, 54, 765, 86, 34];
+let arrayIndex = 0;
+do {
+    console.log(`Values of Array: ${anArray[arrayIndex]}`);
+    arrayIndex = arrayIndex + 1;
+} while (arrayIndex < anArray.length);
