fb0438ad599d892d9e4c3bd80b5e78fbc7e150ac
vijayvansh <sainivijayvansh@gmail.com>
2025-04-29 19:00:37 +0530

    Reduce Method in JS -> Arrays

---
+++ Iterations/reduce.js
@@ -0,0 +1,30 @@
+// Reduce menthod in JS -> Arrays
+
+// myArray.reduce( (accumletor, arrayElement) => {
+//     return accumletor + arrayElement;
+// }, accumletor_initial_value );
+
+myArray = [1, 2, 3, 4, 5];
+
+const total = myArray.reduce((acc, myArrayElement) => {
+    return acc + myArrayElement;
+}, 0);
+
+// console.log(total);
+
+let shoppingCart = [
+    {courseName: "PY course", price: 899},
+    {courseName: "DSA course", price: 1899},
+    {courseName: "JS course", price: 1299},
+    {courseName: "Mobile Dev course", price: 2399}
+];
+
+// const totalPrice = shoppingCart.reduce((acc, course) => acc + course.price, 0);
+
+const totalPrice = shoppingCart.reduce((acc, course) => {
+    return acc + course.price;
+}, 0)
+
+console.log(totalPrice);
+
+
